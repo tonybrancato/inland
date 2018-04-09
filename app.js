1ccd61d3d60c5776a1dfa6b78eeb344831f8aead
Inland Logistics Solutions <inland@inlandls.com>
2018-04-09 12:00:26 -0400

    smooth scroll, copyright date

---
+++ app.js
@@ -0,0 +1,54 @@
+function smoothScroll() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+      
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  }
+  
+  function hideNav() {
+    // hide .navbar first
+    $(".navbar").hide();
+  
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+  
+                  // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 500) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+  }
+  
+  function getYear() {
+    let d = new Date();
+    let n = d.getFullYear();
+    return n;
+  }
+  
+  $(function (){
+    smoothScroll();
+    // hideNav();
+    $( ".copyright" ).append( `<span>&copy; ${getYear()} Inland Logistics Solutions. All Rights Reserved.`); // copyright
+  })
